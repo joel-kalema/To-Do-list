6f16e446f30754d098f303935b5e9663a9000dc9
joel <joelkalema63@gmail.com>
2022-03-23 16:37:52 +0200

    fixed the linters

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import _ from 'lodash';
+import 'lodash';
 import './style.css';
- 
+
 const listsData = [
   {
     description: 'start to learn',
     completed: false,
-    index: 0
+    index: 0,
   },
   {
     description: 'start to learn',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
     description: 'start to learn',
     completed: false,
-    index: 2
+    index: 2,
   },
   {
     description: 'start to learn',
     completed: false,
-    index: 3
-  }
+    index: 3,
+  },
 ];
 const list = document.querySelector('.lists');
 
 listsData.forEach((data) => {
-  list.innerHTML +=`<div class="list">
+  list.innerHTML += `<div class="list">
   <input
     type="checkbox"
     id="task"
@@ -38,4 +38,4 @@ listsData.forEach((data) => {
   <i class="fa fa-trash-o delete" aria-hidden="true"></i>
   <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
 </div>`;
-})
+});
