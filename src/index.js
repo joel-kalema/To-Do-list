a05e6cdf931b1a11b4e8cc9d35f9efc41d3c371e
joel <joelkalema63@gmail.com>
2022-03-23 16:22:21 +0200

    Fixed the linters

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import './style.css';
  
 const listsData = [
   {
-    description: "start to learn",
+    description: 'start to learn',
     completed: false,
     index: 0
   },
   {
-    description: "start to learn",
+    description: 'start to learn',
     completed: false,
     index: 1
   },
   {
-    description: "start to learn",
+    description: 'start to learn',
     completed: false,
     index: 2
   },
   {
-    description: "start to learn microverse",
+    description: 'start to learn',
     completed: false,
     index: 3
   }
@@ -37,5 +37,5 @@ listsData.forEach((data) => {
   <div class="text-area">${data.description}</div>
   <i class="fa fa-trash-o delete" aria-hidden="true"></i>
   <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-</div>`
+</div>`;
 })
