bd04e4c5c16b52a696da00afb25608efb6180593
joel <joelkalema63@gmail.com>
2022-03-24 23:36:25 +0200

    Added the content

---
+++ src/modules/todoTask.js
@@ -0,0 +1,62 @@
+export default class TodoList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask(description) {
+    const completed = false;
+    const id = this.tasks.length + 1;
+    const task = {
+      id,
+      description,
+      completed,
+    };
+    this.tasks.push(task);
+  }
+
+  // removeList(id) {
+  //   this.tasks = this.tasks.filter((task) => {
+  //     if (task.id !== id) return task;
+  //   });
+  // }
+
+  removeList = (id) => {
+    this.tasks.forEach((task) => {
+      if (task.id === id) {
+        this.books.splice(this.books.indexOf(task), 1);
+      }
+    });
+  }
+
+  displayToDo(list) {
+    list.innerHTML = '';
+    this.tasks.forEach((task) => {
+      const li = `<div id="${task.id}" class="list">
+      <input
+        type="checkbox"
+        id="task"
+        name="task"
+        value="task"
+        class="checkbox"
+      />
+      <input
+        type="text" id="text-area" name="task" value="${task.description}" />
+      <button class="delete">
+        <i class="fa fa-trash-o" aria-hidden="true"></i>
+      </button>
+    </div>`;
+      list.innerHTML += li;
+    });
+  }
+
+  setStorage = () => {
+    const formData = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', formData);
+  };
+
+  getStorage = () => {
+    if (localStorage.getItem('tasks')) {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  };
+}
