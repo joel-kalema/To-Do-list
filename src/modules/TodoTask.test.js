a75342e08e5608f78c54ea503a3517252d182653
Olawale <bolawale81@gmail.com>
2022-04-01 11:13:36 +0100

    Update TodoTask.test.js
    Add a test script for the remove function

--- src/modules/TodoTask.test.js
+++ src/modules/TodoTask.test.js
@@ -10,4 +10,10 @@ describe('my TodoList class ', () => {
     kk.addTask("add the task");
     expect(kk.tasks).toHaveLength(++lengthArry);
   });
+  
+  test("remove a task", () => {
+    let lengthArry = kk.tasks.length;
+    kk.removeTask(1);
+    expect(kk.tasks).toHaveLength(--lengthArry);
+  });
 });
